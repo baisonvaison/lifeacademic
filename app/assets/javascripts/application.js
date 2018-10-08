804c32a4bfb9f1bdd94b03d8573d2464434157c6
visonbaison <uume_baison@yahoo.co.jp>
2018-10-08 17:51:23 +0900

    add application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,38 @@
 //= require_tree .
 
 
+$(window).on('load', function() {
+  $(window).scroll(function() {
+    $('.fade-in').each(function() {
+      var POS, scroll, windowHeight;
+      POS = $(this).offset().top;
+      scroll = $(window).scrollTop();
+      windowHeight = $(window).height();
+      if (scroll > POS - windowHeight + windowHeight / 2.0) {
+        $(this).css('opacity', '1');
+      } else {
+        $(this).css('opacity', '0');
+      }
+    });
+  });
+});
+
+
+/*
+ * Back-toTop button (Smooth scroll)
+ */
+
+$(document).on('click', '.back-to-top', function() {
+  $.smoothScroll({
+    easing: 'easeOutExpo',
+    speed: 500
+  });
+});
+
+$(function() {
+  $('a').each(function() {
+    if (window.location.pathname.indexOf($(this).attr('href')) === 0) {
+      $(this).parent().addClass('active');
+    }
+  });
+});
